d834cc570e085d72e94962302874885de7ffbed2
Nathan Costa <ncosta6@umassd.edu>
2024-12-31 13:13:26 -0500

    Update with latest build

---
+++ browser.c61c01ea.js
@@ -0,0 +1,2 @@
+(0,globalThis.parcelRequire94c2.register)("fvaAM",function(e,r){e.exports=function(){throw Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}});
+//# sourceMappingURL=browser.c61c01ea.js.map
